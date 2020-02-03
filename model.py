9e03c8e802402bbbac783016bfcfe944fbbc74bb
tony <tony.sugent@gmail.com>
2020-02-03 13:49:47 -0500

    modified to work with psycopg2
    ready to deploy to AWS

--- model.py
+++ model.py
@@ -24,18 +24,18 @@ class Database:
             generated_electricity = data[i].getGenerated()
             percent_use = data[i].getPercentUse()
             country_code = data[i].getCountryCode()
-            print("""INSERT INTO countries(id,country,reactors,capacity_total,generated_electricity,percent_use,country_code)
+            print("""INSERT INTO nuke_countries(id,country,reactors,capacity_total,generated_electricity,percent_use,country_code)
                  values({}, '{}', '{}', {}, {}, {}, {});""".format(id, country, reactors, capacity_total,
                                                                    generated_electricity, percent_use, country_code))
             self.cursor.execute(
-                """INSERT INTO countries(id,country,reactors,capacity_total,generated_electricity,percent_use,country_code)
+                """INSERT INTO nuke_countries(id,country,reactors,capacity_total,generated_electricity,percent_use,country_code)
                  values({}, '{}', '{}', {}, {}, {}, {});""".format(id, country, reactors, capacity_total,
                                                                    generated_electricity, percent_use, country_code))
             self.c.commit()
 
     def load_countries(self):
         # loads countries for main menu
-        self.cursor.execute('''SELECT country from countries''')
+        self.cursor.execute('''SELECT country from nuke_countries''')
         data = self.cursor.fetchall()
         return data
 
@@ -51,14 +51,14 @@ class Database:
             return False
 
     def country_selec(self, selec):
-        choice = self.cursor.execute('''SELECT * from countries where id = {};'''.format(int(selec)-1))
+        choice = self.cursor.execute('''SELECT * from nuke_countries where id = {};'''.format(int(selec)-1))
         choice = self.cursor.fetchall()
         print(choice)
         return choice
 
     def get_country_codes(self):
         #grab country code and id from country table
-        self.cursor.execute('''SELECT country_code,id from countries''')
+        self.cursor.execute('''SELECT country_code,id from nuke_countries''')
         codes = self.cursor.fetchall()
         return codes
 
@@ -67,20 +67,20 @@ class Database:
         data = s.get_reactors()
 
         for i in range(0, len(data)):
-            self.cursor.execute('''INSERT INTO reactors(id,name,type,status,city,rup,gec,fgc) values({},'{}','{}',
+            self.cursor.execute('''INSERT INTO nuke_reactors(id,name,type,status,city,rup,gec,fgc) values({},'{}','{}',
             '{}','{}',{},{},{})'''.format(data[i].getcountryCode(), data[i].getName(), data[i].getType(), data[i].getStatus(),
                                   data[i].getLocation(), data[i].getRup(), data[i].getGec(), data[i].getFgc()))
             self.c.commit()
 
     def get_reactors(self):
         # get all nuclear reactors.. probably going to use this for stats later
-        self.cursor.execute('''select * from reactors''')
+        self.cursor.execute('''select * from nuke_reactors''')
 
     def get_reactors_country(self, country):
         # get reactors based on country
-        self.cursor.execute('''select id from countries where country ilike '{}' '''.format(country))
+        self.cursor.execute('''select id from countries where nuke_country ilike '{}' '''.format(country))
         countryId = self.cursor.fetchone()
-        self.cursor.execute('''select * from reactors where id = {}'''.format(countryId[0]))
+        self.cursor.execute('''select * from nuke_reactors where id = {}'''.format(countryId[0]))
         reactors = self.cursor.fetchall()
 
         return reactors
